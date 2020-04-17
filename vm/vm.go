58b7aa24e9fb2336220edfcbc10cab117c8d9dcc
Krout0n <krouton@hatena.ne.jp>
2020-04-17 09:45:54 +0900

    ==, !=, >

--- vm/vm.go
+++ vm/vm.go
@@ -52,6 +52,11 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpEqual, code.OpNotEqual, code.OpGreaterThan:
+			err := vm.executeComparison(op)
+			if err != nil {
+				return err
+			}
 		case code.OpPop:
 			vm.pop()
 		case code.OpTrue:
@@ -105,6 +110,49 @@ func (vm *VM) push(obj object.Object) error {
 	vm.stack[vm.sp] = obj
 	vm.sp++
 	return nil
+}
+
+func (vm *VM) executeComparison(op code.Opcode) error {
+	right := vm.pop()
+	left := vm.pop()
+
+	if left.Type() == object.INTEGER_OBJ || right.Type() == object.INTEGER_OBJ {
+		return vm.executeIntegerComparison(op, left, right)
+	}
+
+	switch op {
+	case code.OpEqual:
+		vm.push(nativeBoolToBooleanObject(left == right))
+	case code.OpNotEqual:
+		vm.push(nativeBoolToBooleanObject(left != right))
+	default:
+		return fmt.Errorf("unknown operator: %d (%s %s)", op, left, right)
+	}
+	return nil
+}
+
+func (vm *VM) executeIntegerComparison(op code.Opcode, left, right object.Object) error {
+	leftValue := left.(object.Integer).Value
+	rightValue := right.(object.Integer).Value
+
+	switch op {
+	case code.OpEqual:
+		vm.push(nativeBoolToBooleanObject(leftValue == rightValue))
+	case code.OpNotEqual:
+		vm.push(nativeBoolToBooleanObject(leftValue != rightValue))
+	case code.OpGreaterThan:
+		vm.push(nativeBoolToBooleanObject(leftValue > rightValue))
+	default:
+		return fmt.Errorf("unknown operator: %d (%s %s)", op, left, right)
+	}
+	return nil
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return True
+	}
+	return False
 }
 
 func (vm *VM) pop() object.Object {
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -55,6 +55,8 @@ func TestBooleanExpressions(t *testing.T) {
 	tests := []vmTestcase{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true}, {"1 > 2", false}, {"1 < 1", false}, {"1 > 1", false}, {"1 == 1", true}, {"1 != 1", false}, {"1 == 2", false}, {"1 != 2", true}, {"true == true", true}, {"false == false", true}, {"true == false", false}, {"true != false", true}, {"false != true", true},
+		{"(1 < 2) == true", true}, {"(1 < 2) == false", false}, {"(1 > 2) == true", false}, {"(1 > 2) == false", true},
 	}
 
 	runVMTests(t, tests)
